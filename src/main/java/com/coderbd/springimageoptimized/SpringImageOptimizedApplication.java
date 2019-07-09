b21e8ab688db2ee8e6363d1e3e3f839456d256af
Mohammad Rajaul Islam <springapidev@gmail.com>
2019-07-09 14:50:53 +0600

    image upload with spring boot web app

---
+++ src/main/java/com/coderbd/springimageoptimized/SpringImageOptimizedApplication.java
@@ -0,0 +1,13 @@
+package com.coderbd.springimageoptimized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringImageOptimizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringImageOptimizedApplication.class, args);
+	}
+
+}
